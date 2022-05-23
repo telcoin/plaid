37ccbae128f41136bceabf02ebeca33f13ae7043
Walker Thornley <walker.thornley@telco.in>
2022-05-23 13:59:07 -0700

    Add min datetime to balance request

--- src/types/account.rs
+++ src/types/account.rs
@@ -389,4 +389,16 @@ pub struct BalanceRequestOptions {
     /// associated with the Item.
     #[serde(default, with = "super::serde_utils::default_on_null")]
     pub account_ids: Vec<String>,
+
+    /// Timestamp in ISO 8601 format (YYYY-MM-DDTHH:mm:ssZ) indicating the oldest acceptable balance
+    /// when making a request to /accounts/balance/get.
+    ///
+    /// If the balance that is pulled for ins_128026 (Capital One) is older than the given timestamp,
+    /// an INVALID_REQUEST error with the code of LAST_UPDATED_DATETIME_OUT_OF_RANGE will be returned
+    /// with the most recent timestamp for the requested account contained in the response.
+    ///
+    /// This field is only used when the institution is ins_128026 (Capital One), in which case a
+    /// value must be provided or an INVALID_REQUEST error with the code of INVALID_FIELD will be
+    /// returned. For all other institutions, this field is ignored.
+    pub min_last_updated_datetime: Option<String>,
 }
